1ed19709e3e7abca9a4ec00bac85c60206cf8a35
Fikri Firman Fadilah <fikrifirmanfadilah@gmail.com>
2020-12-30 18:50:05 +0700

    fixxxxxxxxx

--- scrapper.js
+++ scrapper.js
@@ -3,7 +3,8 @@ const config = require("./config.json")
 
 
 const scrapeLeague = async (query) => {
-    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox','--disable-setuid-sandbox']})
+    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox','--disable-setuid-sandbox','--disable-dev-shm-usage',
+    '--single-process']})
     const page = await browser.newPage()
     if (query == "pl") {
         await page.goto('https://www.theguardian.com/football/' + config.pl.uniqueName + '/table')
